35b1b475a39e3907873b934141496ab85561730d
Aashishh05 <shresthaaashish246@gmail.com>
2026-06-09 19:30:30 +0545

    added authentication routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-
+import mainRoutes from "./routes/mainRoutes.js"
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 connectDB();
 const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api",mainRoutes)
 
 app.get("/", (req, res) => {
   res.json({ message: "API is running......" });
