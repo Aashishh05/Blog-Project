f6fbaf53802ed61bdc7eb5f390ee255cc4506408
Aashishh05 <shresthaaashish246@gmail.com>
2026-06-12 14:53:08 +0545

    upload middlewaare fixed

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -1,11 +1,11 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
-    cb(null, "/uploads");
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
   },
 
-  filename: function (req, res, cb) {
+  filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
