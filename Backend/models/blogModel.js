f49263a9d8c92a6d57fc96beb022ee8f3e24e809
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-11 18:16:33 +0545

    fix category model name mismatch

--- Backend/models/blogModel.js
+++ Backend/models/blogModel.js
@@ -26,7 +26,7 @@ const blogSchema = new mongoose.Schema(
 
     category: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
+      ref: "category",
       required: true,
     },
 
