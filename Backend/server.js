71a998983c56b6dcafb0f0c3d98a9e9439592f02
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-11 17:20:09 +0545

    error fixed

--- Backend/server.js
+++ Backend/server.js
@@ -17,11 +17,11 @@ app.use(
     origin: (origin, callback) => {
       const allowedOrigins = [
         "http://localhost:5173",
-
-        "blog-project-u4rd-dusky.vercel.app",
-        "blog-project-u4rd-git-main-aashish11.vercel.app",
-        "blog-project-u4rd-6ehmykmw0-aashish11.vercel.app",
+        "https://blog-project-u4rd-dusky.vercel.app",
+        "https://blog-project-u4rd-git-main-aashish11.vercel.app",
+        "https://blog-project-u4rd-6ehmykmw0-aashish11.vercel.app",
       ];
+
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
